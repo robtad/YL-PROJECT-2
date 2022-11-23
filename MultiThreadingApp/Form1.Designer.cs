7d71fb921d600b8e33f33ab4085510f45a046498
ROBERA TADESSE <91320577+robtad@users.noreply.github.com>
2022-11-23 22:18:20 +0300

    Initial commit

---
+++ MultiThreadingApp/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MultiThreadingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(285, 450);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(291, 76);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(559, 194);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(708, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ProductSimilarity";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBox
+            // 
+            this.tBox.Location = new System.Drawing.Point(291, 276);
+            this.tBox.Multiline = true;
+            this.tBox.Name = "tBox";
+            this.tBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tBox.Size = new System.Drawing.Size(324, 174);
+            this.tBox.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Complaint ID",
+            "Product",
+            "Issue",
+            "Company",
+            "State",
+            "ZIP code"});
+            this.comboBox1.Location = new System.Drawing.Point(291, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Select Column";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            ">=",
+            "<=",
+            "=="});
+            this.comboBox2.Location = new System.Drawing.Point(428, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.Text = "Select Inequality";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(564, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "Enter Similarity %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 499);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MultiThreadingApp/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreadingApp
+{
+    public partial class Form1 : Form
+    {
+        public static DataTable Csv_to_table(string CSVFilePathName)
+        {
+            string[] Lines = File.ReadAllLines(CSVFilePathName);
+            string[] Fields;
+            Fields = Lines[0].Split(new char[] { ',' });
+            int Cols = Fields.GetLength(0);
+            DataTable dt = new DataTable();
+            //1st row must be column names; force lower case to ensure matching later on.
+            for (int i = 0; i < Cols; i++)
+                dt.Columns.Add(Fields[i].ToLower(), typeof(string));
+            DataRow Row;
+            for (int i = 1; i < Lines.GetLength(0); i++)
+            {
+                Fields = Lines[i].Split(new char[] { ',' });
+                Row = dt.NewRow();
+                for (int f = 0; f < Cols; f++)
+                    Row[f] = Fields[f];
+                dt.Rows.Add(Row);
+            }
+
+            return dt;
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //var dt = ConvertCSVtoDataTable(@"C:\Users\RobTad\Documents\KoU\YAZLAB-1\P2\sample.csv");
+            var dt = Csv_to_table(@"C:\Users\RobTad\Documents\KoU\YAZILIMLAB-1\YL-PROJECT-2\small_sample.csv");
+            //dataGridView1.DataSource = dt;
+            
+        }
+
+
+        public DataTable dt = Csv_to_table(@"C:\Users\RobTad\Documents\KoU\YAZILIMLAB-1\YL-PROJECT-2\small_sample.csv");
+        public DataTable dtNew;
+        //public DataTable dtDisplay =  new DataTable();
+        
+
+        public HashSet<string> splitter(int i,int column, DataTable dt)
+        {
+            string target;
+            target = (string)dt.Rows[i][column];
+            string[] splitted_target;
+            splitted_target = target.Split(' ');
+            var target_set = new HashSet<string>(splitted_target);
+            //tBox.AppendText(string.Join(", ", splitted_target));
+            //tBox.AppendText(string.Join(", ", target_set));
+            
+            return target_set;
+        }
+
+        public int ListLen(int i, int column, DataTable dt)
+        {
+            string target;
+            target = (string)dt.Rows[i][column];
+            string[] splitted_target;
+            splitted_target = target.Split(' ');
+            return splitted_target.Length;
+        }
+        
+        //writing results to datagridview
+        public void AddResult(int i, int j)
+        {
+
+            //dtNew = dt.Clone();
+            //dtNew.ImportRow(row);
+            //row == dt.rows[i]
+            //row == dt.rows[j]
+            //dtNew.Rows.Add(dt.Rows[i]);
+            //dtNew.Rows.Add(dt.Rows[j]);
+
+            dtNew.ImportRow(dt.Rows[i]);
+            dtNew.ImportRow(dt.Rows[j]);
+
+            //dataGridView2.DataSource = dtNew;
+
+        }
+
+        public void check_similarity(int column, int inequality_flag, float similarity_percentage)
+        {
+            dtNew = dt.Clone();//datatable for the outputs of similarity check
+                               //add columns for dtDisplay
+            DataTable dtDisplay =  new DataTable();
+
+            dtDisplay.Columns.Add("KAYIT 1", typeof(String));
+            dtDisplay.Columns.Add("KAYIT 2", typeof(String));
+            dtDisplay.Columns.Add("BENZERLİK ORANI", typeof(String));
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                //column designates the column to be checked for similarity(product or issue)
+                
+                var set1 = splitter(i, column, dt);
+
+                for (int j = i+1; j < dt.Rows.Count; j++)
+                {
+                    var set2 = splitter(j, column, dt);
+                    //check intersection between set1 and set2
+                    HashSet<string> intersection = new HashSet<string>(set1);
+                    intersection.IntersectWith(set2);
+                    //calculate percentage depending on intersection
+                    if (intersection.Count>0)
+                    {
+                        float len1_n_len2 = intersection.Count();
+                        tBox.AppendText(string.Join(", ", intersection));
+                        tBox.AppendText(Environment.NewLine);
+                        //tBox.AppendText("intersection len = " + len1_n_len2);
+
+                        float len1 = ListLen(i, column, dt);
+                        float len2 = ListLen(j, column, dt);
+                        float longer = len1 >= len2 ? len1 : len2;
+                        float percentage = (len1_n_len2 / longer)*100;
+                        percentage = (float)Math.Round(percentage, 1);
+
+                        if (percentage >= similarity_percentage)
+                        {
+                            
+                            tBox.AppendText("intersection len = " + len1_n_len2 + "-> " + len1 + ", " + len2);
+                            tBox.AppendText(Environment.NewLine);
+                            tBox.AppendText("intersection percentage = " + percentage + "%");
+                            tBox.AppendText(Environment.NewLine);
+                            tBox.AppendText("(i,j) = (" + i + "," + j + ")");
+                            tBox.AppendText(Environment.NewLine);
+                            string percent = percentage + "%";
+                            AddResult(i, j);
+
+                            //MyTable.Rows.Add(MyTable2.Rows[0]["Id"], MyTable2.Rows[0]["Name"]);
+                            dtDisplay.Rows.Add(dt.Rows[i][column], dt.Rows[j][column], percentage + "%");
+
+                            //write the following at the end of outer for loop
+                            
+                            dtNew.ImportRow(dt.Rows[i]);
+                            dtNew.ImportRow(dt.Rows[j]);
+
+                            //dataGridView1.DataSource = dtNew;
+                             
+                        }
+
+
+
+                    }
+
+
+                }
+
+
+            }
+            dataGridView1.DataSource = dtNew;
+            dataGridView2.DataSource = dtDisplay;
+
+
+        }
+        //DataTable containing all the csv records (dt)
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tBox.Clear();
+            
+            //dataGridView2.DataSource = dtNew;
+            //getting column to be checked from a user (using combobox's dropdown list)
+            if (!float.TryParse(textBox2.Text, out float similarity_percentage))
+            {
+                MessageBox.Show("Please enter a number for similarity percentage");
+                return;
+            }
+            if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+            {
+                MessageBox.Show("Wrong Column or Inequality Operator!\nTry Again!");
+                return;
+            }
+            //float similarity_percentage = float.Parse(textBox2.Text);
+            int column = comboBox1.SelectedIndex;
+            int inequality = comboBox2.SelectedIndex;
+
+            check_similarity(column, inequality, similarity_percentage);
+
+             
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            int index = e.RowIndex;
+            //dtNew.Rows.Clear();
+            dataGridView1.DataSource = dtNew.Rows[index];
+            dataGridView1.DataSource = dtNew.Rows[index+1];
+
+
+            //dataGridView1
+
+        }
+    }
+}
